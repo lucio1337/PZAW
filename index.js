30cb1d323e9b89dd96c0db4548b7a7cd1143b999
Lucio <lfurmann6@gmail.com>
2025-10-07 23:44:12 +0200

    Dodanie i zmodyfikowanie plików do serwera HTTP z favicon

---
+++ index.js
@@ -0,0 +1,22 @@
+import { createServer } from "node:http";
+import { URL } from "node:url";
+import { handlePath } from "./src/path_handlers.js";
+
+const server = createServer((req, res) => {
+  const request_url = new URL(`http://${host}${req.url}`);
+  console.log(`Request: ${req.method} ${request_url.pathname}`);
+
+  handlePath(request_url.pathname, req, res);
+
+  if (!res.writableEnded) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Nie znaleziono strony\n");
+  }
+});
+
+const port = 8000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+  console.log(`Server listening on http://${host}:${port}`);
+});
---
+++ src/path_handlers.js
@@ -0,0 +1,37 @@
+import { readFileSync } from "node:fs";
+
+const index_html = readFileSync("static/index.html");
+const favicon = readFileSync("favicon.ico");
+
+const pathConfigs = [
+  {
+    path: "/",
+    allowed_methods: ["GET"],
+    handler: (req, res) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(index_html);
+    },
+  },
+  {
+    path: "/favicon.ico",
+    allowed_methods: ["GET"],
+    handler: (req, res) => {
+      res.writeHead(200,{ "Content-Type": "image/x-icon"});
+      res.end(favicon);
+    },
+  },
+];
+
+export function handlePath(path, req, res) {
+  for (let config of pathConfigs) {
+    if (path === config.path) {
+      if (config.allowed_methods.includes(req.method)) {
+        config.handler(req, res);
+      } else {
+        res.writeHead(405, { "Content-Type": "text/plain" });
+        res.end("Niedozwolona metoda\n");
+      }
+      break;
+    }
+  }
+}
