e96f7aa21c9ffde2c17fc9164ebec0801a2339da
Lucio <lfurmann6@gmail.com>
2026-03-06 22:08:16 +0100

    Poprawinie błędu ze zmeczenia

--- projekt04/database.js
+++ projekt04/database.js
@@ -1,9 +1,7 @@
 import Database from 'better-sqlite3';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import { initializeDatabase } from "./database.js";
 
-initializeDatabase();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
--- projekt04/seeder.js
+++ projekt04/seeder.js
@@ -1,6 +1,9 @@
 import Database from 'better-sqlite3';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { initializeDatabase } from "./database.js";
+
+initializeDatabase();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
