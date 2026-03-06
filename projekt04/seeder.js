ad8a3b77f9c8febb19850134b8ae5447bde94e18
Lucio <lfurmann6@gmail.com>
2026-03-06 21:26:54 +0100

    Gruntowny redesign strony oraz uzupełnienie README

--- projekt04/seeder.js
+++ projekt04/seeder.js
@@ -19,20 +19,20 @@ db.exec(`INSERT INTO categories (id, title, required_fields) VALUES
   ('ulubieni-artysci', 'ulubieni artyści', '["wykonawca","ocena"]')
 `);
 
-db.exec(`INSERT INTO cards (category_id, tytuł, wykonawca, gatunek, ocena) VALUES
-  ('ulubione-albumy', 'Dawn FM', 'The Weeknd', 'synthpop, dance-pop', '9'),
-  ('ulubione-albumy', 'Random Access Memories', 'Daft Punk', 'electronic, disco', '9.5'),
-  ('ulubione-utwory', 'Paranoid', 'Black Sabbath', 'heavy metal', '8.5'),
-  ('ulubione-utwory', 'Początek', 'Męskie Granie Orkiestra, Dawid Podsiadło, Kortez, Krzysztof Zalewski', 'pop', '10'),
-  ('ulubione-utwory', 'Bohemian Rhapsody', 'Queen', 'rock, progressive rock', '10'),
-  ('ulubione-utwory', 'Stairway to Heaven', 'Led Zeppelin', 'rock', '9.8'),
-  ('ulubieni-artysci', NULL, 'Michael Jackson', NULL, '9.7'),
-  ('ulubieni-artysci', NULL, 'Metallica', NULL, '8.4'),
-  ('ulubieni-artysci', NULL, 'The Beatles', NULL, '9.9')
+db.exec(`INSERT INTO cards (category_id, tytuł, wykonawca, gatunek, ocena, user_id) VALUES
+  ('ulubione-albumy', 'Dawn FM', 'The Weeknd', 'synthpop, dance-pop', '9',1),
+  ('ulubione-albumy', 'Random Access Memories', 'Daft Punk', 'electronic, disco', '9.5',1),
+  ('ulubione-utwory', 'Paranoid', 'Black Sabbath', 'heavy metal', '8.5',1),
+  ('ulubione-utwory', 'Początek', 'Męskie Granie Orkiestra, Dawid Podsiadło, Kortez, Krzysztof Zalewski', 'pop', '10',1),
+  ('ulubione-utwory', 'Bohemian Rhapsody', 'Queen', 'rock, progressive rock', '10',1),
+  ('ulubione-utwory', 'Stairway to Heaven', 'Led Zeppelin', 'rock', '9.8',1),
+  ('ulubieni-artysci', NULL, 'Michael Jackson', NULL, '9.7',1),
+  ('ulubieni-artysci', NULL, 'Metallica', NULL, '8.4',1),
+  ('ulubieni-artysci', NULL, 'The Beatles', NULL, '9.9',1)
 `);
 
-const playlist1 = db.prepare('INSERT INTO playlists (name) VALUES (?)').run('Rock Classics');
-const playlist2 = db.prepare('INSERT INTO playlists (name) VALUES (?)').run('Best of All Time');
+const playlist1 = db.prepare('INSERT INTO playlists (name,user_id) VALUES (?,1)').run('Rock Classics');
+const playlist2 = db.prepare('INSERT INTO playlists (name,user_id) VALUES (?,1)').run('Best of All Time');
 
 const addSong = db.prepare('INSERT INTO playlist_songs (playlist_id, song_tytuł) VALUES (?, ?)');
 
